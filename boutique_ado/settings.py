3e5f1c8a9dda0e6b34f68bcb9ea893793f537f91
SimGriff <simgrif@gmail.com>
2022-08-11 20:14:38 +0000

    DEBUG change

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import dj_database_url
 from pathlib import Path
+import dj_database_url
 if os.path.exists("env.py"):
     import env
 
 
-# development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT', True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = development
 
 ALLOWED_HOSTS = ['sim-boutique-ado.herokuapp.com', 'localhost']
 
